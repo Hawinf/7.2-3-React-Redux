d9721b49e3f4a4745947e32e1c853efa42d7aec3
Hawinf <hawinf54@gmail.com>
2023-08-16 18:34:13 +0400

    app.js store.js index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const [num, setNum] = useState(0)
+
+  const handlePlus = () => {
+    setNum((prevState) => prevState + 1)
+  }
+  const handleMines = () => {
+    setNum((prevState) => prevState - 1)
+  }
+
+  // How to receive data from redux aka store
+  const { total } = useSelector((state) => state)
+  console.log(total)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handlePlus}>+</button>
+      <button onClick={handleMines}>-</button>
+      <h1>{num}</h1>
     </div>
   );
 }
---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { createStore } from "redux";
+
+const counterState = {
+    total: 20,
+}
+const counterReducer = (state = counterState) => {
+    return state;
+}
+
+const store = createStore(counterReducer);
+
+export default store;
